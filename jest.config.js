446fef047fa6690cbc9e5890f4d7a27b38cbd087
Assper <TheAssper@gmail.com>
2019-04-22 00:26:44 +0300

    Add app tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  clearMocks: true,
+  'moduleDirectories': [
+    'node_modules',
+    'app'
+  ],
+  'moduleFileExtensions': [
+    'js',
+    'jsx'
+  ],
+  testEnvironment: 'node',
+  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime']
+}
---
+++ tests/backend/helpers.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+import enableDestroy from 'server-destroy'
+import config from '../../src/backend/config'
+import Token from '../../src/backend/models/Token'
+import User from '../../src/backend/models/User'
+
+export async function databaseDisconnect() {
+  if (mongoose.connection.db) {
+    await mongoose.connection.close()
+  }
+}
+
+export async function databaseConnect() {
+  const { name, host, port } = config
+  await databaseDisconnect()
+  mongoose.Promise = global.Promise
+  await mongoose.connect(`mongodb://${host}:${port}/${name}`, { useNewUrlParser: true })
+}
+
+export async function databaseCleanup() {
+  await Token.remove()
+  await User.remove()
+}
+
+export function testAsync(fn) {
+  return async () => {
+    try {
+      await fn()
+    } catch (err) {
+      throw `TEST ASYNC ERROR: ${err}`
+    }
+  }
+}
+
+export function destroyServer(server) {
+  enableDestroy(server)
+  server.destroy()
+}
